8f37dc28a48e01772813a8dd4e1e8ecd477ee3a1
Manfred Touron <m@42.am>
2019-09-24 18:12:49 +0200

    feat: MVP

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module moul.io/hacker-typing
 
 go 1.13
+
+require (
+	github.com/eiannone/keyboard v0.0.0-20190314115158-7169d0afeb4f
+	golang.org/x/sys v0.0.0-20190924135425-2f72d4f06240 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/eiannone/keyboard"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	log.SetFlags(0)
+	if len(os.Args) < 2 {
+		log.Print("usage: hacker-typing path/to/file")
+		os.Exit(1)
+	}
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	content, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := keyboard.Open(); err != nil {
+		log.Fatal(err)
+	}
+	defer keyboard.Close()
+
+	i := 0
+	for {
+		_, key, err := keyboard.GetKey()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if key == keyboard.KeyEsc {
+			break
+		}
+		if i < len(content) {
+			fmt.Printf("%c", content[i])
+		}
+		i++
+	}
 }
